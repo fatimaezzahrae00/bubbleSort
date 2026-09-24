43df611f5e2a77718ea2c19e6710d02caa2d2181
fatimaezzahrae00 <ffatym44@gmail.com>
2026-09-24 16:59:25 +0200

    Add files via upload

---
+++ bubble.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    
+  const n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+}
+console.log(bubbleSort([200 ,5 ,90 , 100]));
